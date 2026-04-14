e6c0fc3f9e10900af88cd46200cc2551a41014b5
ramonaljr <ramonvallejerajr@gmail.com>
2026-04-14 14:55:20 +0800

    feat(auth): apply AuthShell to sign-in and sign-up routes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,22 @@
+import Link from 'next/link';
 import { SignUp } from '@clerk/nextjs';
+import AuthShell from '@/components/auth/AuthShell';
+import { clerkAppearance } from '@/components/auth/clerkAppearance';
+
 export default function Page() {
-  return <SignUp />;
+  return (
+    <AuthShell
+      title="Create your account"
+      footerSlot={
+        <span>
+          Already have one?{' '}
+          <Link href="/sign-in" className="text-[#6b7fff] hover:text-[#93c5fd]">
+            Sign in
+          </Link>
+        </span>
+      }
+    >
+      <SignUp appearance={clerkAppearance} />
+    </AuthShell>
+  );
 }
